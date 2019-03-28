c2fb81fc1283371d3abc1aad4e367ccc88c5b4ab
ray <raymond@spriing.co.uk>
2019-03-28 17:13:11 +0000

    Added android injector into class

--- app/src/main/java/com/rayt/baseapplication/MainActivity.kt
+++ app/src/main/java/com/rayt/baseapplication/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.rayt.baseapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.android.AndroidInjection
+import dagger.android.support.DaggerAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
